b8760674c1d4d48d334ea860ea001c14175891d3
neetujaayapal <neetujaayapal16.com>
2022-06-03 00:52:11 -0400

    Revised

--- src/AssignmentLoopsAndScanners/FahrenHeatToCelsius.java
+++ src/AssignmentLoopsAndScanners/FahrenHeatToCelsius.java
@@ -5,18 +5,29 @@ import java.util.Scanner;
 public class FahrenHeatToCelsius {
 
 	public static void main(String[] args) {
-		// Convert the temp from Celsius to Fahrenheat
-		//Celsius to Fahrenheat:  (0°C × 9/5) + 32 = 32°F
+	//  program that can convert degrees Fahrenheit to degrees Celsius, or vice versa
+		
+		Scanner sc = new Scanner (System.in);
+		
+		String unit = ""; //C ,F
+		System.out.println("Enter the unit");
+		unit = sc.next();
 		
 		double temperature = 0;
-		System.out.println("Enter the temperature in Celsius");
-		Scanner sc = new Scanner (System.in);
+		System.out.println("Enter the temperature");
 		temperature = sc.nextDouble();
-		
-	    double tempInFahren = (temperature*9/5)+32;
-	    System.out.println("Temperature in Fahren Heat = ("+temperature+ "°C x 9/5) + 32 ="+tempInFahren+ "°F" );
+		if (unit.equals("C")) {
+		double tempInFahrenHeat = (temperature*9/5)+32;
+		System.out.println("Temperature in fahren heat =("+temperature+ "X  9/5)  + 32  =" +tempInFahrenHeat+ "°F" );
+		System.out.println("Temperature in fahren heat = "+Math.round(tempInFahrenHeat)+ "°F");
+		}
+		else if (unit.equals("F")) {
+			double tempInCelsius = (temperature-32)*5/9;
+			System.out.println("Temperature in Celsius =("+temperature+ " - 32)  x 5/9  =" +tempInCelsius+ "°C");
+			System.out.println("Temperature in Celsius = "+Math.round(tempInCelsius)+ "°C");
+		}
 	    sc.close();
-	
+		
 	
 	
 	
--- src/AssignmentLoopsAndScanners/TemperatureTable.java
+++ src/AssignmentLoopsAndScanners/TemperatureTable.java
@@ -5,42 +5,38 @@ import java.util.Scanner;
 public class TemperatureTable {
 
 	public static void main(String[] args) {
-		/* When the user enter 0 temperature, the below table should be dispalyed
-		 * -20.00        -4.00
-		 * -15.00         5.00
-		 * -10.00        14.00
+		/*
+		 * When the user enter 0 temperature, the below table should be dispalyed -20.00
+		 * -4.00 -15.00 5.00 -10.00 14.00
 		 * 
-		 * -5.00        23.00
-		 * 0.00        32.00
-		 * 5.00        41.00
+		 * -5.00 23.00 0.00 32.00 5.00 41.00
 		 * 
-		 * 10.00        50.00
-		 * 15.00        59.00
-		 * 20.00        68.00
-		 *25.00        77.00
+		 * 10.00 50.00 15.00 59.00 20.00 68.00 25.00 77.00
+		 *
+		 *
 		 */
-		
-		double currentTemperature = 0;
+		Scanner sc = new Scanner(System.in);
+
+		String unit = ""; // C ,F
+		System.out.println("Enter the unit");
+		unit = sc.next();
+
+		double temperature = 0;
 		System.out.println("Enter the temperature");
-		Scanner sc = new Scanner (System.in);
-		currentTemperature = sc.nextDouble();
-		
+		temperature = sc.nextDouble();
+		if (unit.equals("C")) {
+	
 		System.out.println("Celsius");
-	    
-	    for (int temp =-20; temp<=25;temp+=5) {
-	  
-	    System.out.println(+temp);
-	    }
-	    
-	    System.out.println("Fahren Heat");
-	     for (int temperature = -4 ; temperature<=77 ;temperature+=9) {
-	    	 System.out.println(+temperature);
-	     }
-	    
-	     sc.close();
-	
-	
-	
+		for (int temp = -20; temp <= 25; temp += 5) {
+		System.out.println(+temp);
+		}
+		} else if (unit.equals("F")) {
+		 		System.out.println("Fahren Heat");
+		for (int temp = -4; temp <= 77; temp += 9) {
+		System.out.println(+temp);
+			}
+		}
+		sc.close();
 
 	}
 
